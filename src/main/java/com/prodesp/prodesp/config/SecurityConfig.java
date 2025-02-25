88388a750aea6666903f473dfbfd8248e11fc92c
fellypeavelino <fellypeavelino@hotmail.com>
2025-02-24 23:07:30 -0300

    desabilitando interceptor e configurando spring security

---
+++ src/main/java/com/prodesp/prodesp/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.prodesp.prodesp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ *
+ * @author Usuario
+ */
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                .anyRequest().authenticated()
+        ).formLogin().disable().httpBasic();
+        return http.build();
+    }
+}
