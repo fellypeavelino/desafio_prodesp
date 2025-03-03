f84218c54f62b14221d28502fa10e56d6cef9bf5
fellypeavelino <fellypeavelino@hotmail.com>
2025-03-03 10:43:21 -0300

    adicionando cor no seed

--- src/main/java/com/prodesp/prodesp/config/DatabaseInitializer.java
+++ src/main/java/com/prodesp/prodesp/config/DatabaseInitializer.java
@@ -5,6 +5,7 @@ import com.prodesp.prodesp.entities.Categorias;
 import com.prodesp.prodesp.entities.Usuarios;
 import com.prodesp.prodesp.repositories.CategoriasRepository;
 import com.prodesp.prodesp.repositories.UsuariosRepository;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,13 +48,19 @@ public class DatabaseInitializer implements CommandLineRunner {
             logger.warn("Usuários já existentes no banco de dados.");
         }
         if (categoriasRepository.count() == 0) {
-            String[] statusCategoria = {"Em espera", "Priorizado", "Iniciado", "Concluído"};
-            for (String string : statusCategoria) {
-                Categorias c = new Categorias();
-                c.setCor("#24dba4");
-                c.setNome(string);
-                categoriasRepository.save(c);
-            }
+            Map<String, String> categoriasComCores = Map.of(
+                "Em espera", "#FFC107",   // Amarelo
+                "Priorizado", "#FF5722",  // Laranja
+                "Iniciado", "#2196F3",    // Azul
+                "Concluído", "#4CAF50"    // Verde
+            );
+
+            categoriasComCores.forEach((nome, cor) -> {
+                Categorias categoria = new Categorias();
+                categoria.setNome(nome);
+                categoria.setCor(cor);
+                categoriasRepository.save(categoria);
+            });
         } else {
             logger.warn("Categorias já existentes no banco de dados.");
         }
